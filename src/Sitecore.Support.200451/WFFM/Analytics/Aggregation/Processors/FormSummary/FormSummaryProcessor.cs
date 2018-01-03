39210e937a80774255438e36e30bbdfecb9c378c
DK\AMCH <amch@sitecore.net>
2018-01-03 10:06:31 +0200

    commit patch

--- src/Sitecore.Support.200451/WFFM/Analytics/Aggregation/Processors/FormSummary/FormSummaryProcessor.cs
+++ src/Sitecore.Support.200451/WFFM/Analytics/Aggregation/Processors/FormSummary/FormSummaryProcessor.cs
@@ -11,72 +11,72 @@ using Sitecore.WFFM.Analytics.Aggregation.Processors.FormSummary;
 
 namespace Sitecore.Support.WFFM.Analytics.Aggregation.Processors.FormSummary
 {
-  /// <summary>
-  /// Defines form summary processor class.
-  /// </summary>
-  public class FormSummaryProcessor : InteractionAggregationPipelineProcessor
-  {
     /// <summary>
-    /// Processes form summary.
+    /// Defines form summary processor class.
     /// </summary>
-    /// <param name="args">Aggregation pipeline arguments.</param>
-    protected override void OnProcess(InteractionAggregationPipelineArgs args)
+    public class FormSummaryProcessor : InteractionAggregationPipelineProcessor
     {
-      Assert.ArgumentNotNull(args, nameof(args));
-
-      var contact = args.Context.Contact;
-      var interaction = args.Context.Interaction;
-
-      var pageEvents = args.Context.Interaction.Events.Where(e => e.DefinitionId == PageEventIds.SubmitSuccessEvent);
-
-      if (!pageEvents.Any() || contact.Id == null || interaction.Id == null)
-      {
-        return;
-      }
-
-      foreach (var pageEvent in pageEvents)
-      {
-        var formSummaryItem = args.Context.Results.GetFact<Sitecore.WFFM.Analytics.Aggregation.Processors.FormSummary.FormSummary>();
-
-        var formSummaryKey = new FormSummaryKey()
+        /// <summary>
+        /// Processes form summary.
+        /// </summary>
+        /// <param name="args">Aggregation pipeline arguments.</param>
+        protected override void OnProcess(InteractionAggregationPipelineArgs args)
         {
-          Id = Guid.NewGuid(),
-          ContactId = contact.Id.Value,
-          FormId = pageEvent.ItemId,
-          InteractionId = interaction.Id.Value,
-          Created = pageEvent.Timestamp
-        };
+            Assert.ArgumentNotNull(args, nameof(args));
 
-        var formSummaryValue = new FormSummaryValue()
-        {
-          Count = 1
-        };
+            var contact = args.Context.Contact;
+            var interaction = args.Context.Interaction;
+            #region start modified part of code, added "e.Data != null" condition
+            var pageEvents = args.Context.Interaction.Events.Where(e => (e.DefinitionId == PageEventIds.SubmitSuccessEvent && e.Data != null));
+            #endregion 
+            if (!pageEvents.Any() || contact.Id == null || interaction.Id == null)
+            {
+                return;
+            }
 
-        formSummaryItem.Emit(formSummaryKey, formSummaryValue);
+            foreach (var pageEvent in pageEvents)
+            {
+                var formSummaryItem = args.Context.Results.GetFact<Sitecore.WFFM.Analytics.Aggregation.Processors.FormSummary.FormSummary>();
 
-        List<FieldData> fields;
-        using (TextReader tr = new StringReader(pageEvent.Data))
-        {
-          fields = new XmlSerializer(typeof(List<FieldData>)).Deserialize(tr) as List<FieldData>;
+                var formSummaryKey = new FormSummaryKey()
+                {
+                    Id = Guid.NewGuid(),
+                    ContactId = contact.Id.Value,
+                    FormId = pageEvent.ItemId,
+                    InteractionId = interaction.Id.Value,
+                    Created = pageEvent.Timestamp
+                };
+
+                var formSummaryValue = new FormSummaryValue()
+                {
+                    Count = 1
+                };
+
+                formSummaryItem.Emit(formSummaryKey, formSummaryValue);
+
+                List<FieldData> fields;
+                using (TextReader tr = new StringReader(pageEvent.Data))
+                {
+                    fields = new XmlSerializer(typeof(List<FieldData>)).Deserialize(tr) as List<FieldData>;
+                }
+
+                if (fields == null)
+                {
+                    continue;
+                }
+
+                foreach (var field in fields)
+                {
+                    IEnumerable<string> fieldValues = field.Values != null && field.Values.Count > 0 ? field.Values :
+                        (!string.IsNullOrEmpty(field.Value) ? Enumerable.Repeat(field.Value, 1) : Enumerable.Repeat(string.Empty, 1));
+
+                    foreach (string fieldValue in fieldValues)
+                    {
+                        var formFieldValue = args.Context.Results.GetDimension<Sitecore.WFFM.Analytics.Aggregation.Processors.FormFieldValues.FormFieldValues>();
+                        formFieldValue.Add(formSummaryKey.Id, field.FieldId, field.FieldName, fieldValue);
+                    }
+                }
+            }
         }
-
-        if (fields == null)
-        {
-          continue;
-        }
-
-        foreach (var field in fields)
-        {
-          IEnumerable<string> fieldValues = field.Values != null && field.Values.Count > 0 ? field.Values :
-              (!string.IsNullOrEmpty(field.Value) ? Enumerable.Repeat(field.Value, 1) : Enumerable.Repeat(string.Empty, 1));
-
-          foreach (string fieldValue in fieldValues)
-          {
-            var formFieldValue = args.Context.Results.GetDimension<Sitecore.WFFM.Analytics.Aggregation.Processors.FormFieldValues.FormFieldValues>();
-            formFieldValue.Add(formSummaryKey.Id, field.FieldId, field.FieldName, fieldValue);
-          }
-        }
-      }
     }
-  }
 }
